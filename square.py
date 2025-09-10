507f0e7b6d561167b0635db55cc89aac94da58c6
[parikshit yadav] <[pareekshityadav@gmail.com]>
2025-09-10 22:32:00 +0530

    square_pyhton

---
+++ square.py
@@ -0,0 +1,11 @@
+# Python script to calculate square of a number
+
+# Ask user for input
+num = int(input("Enter a number: "))
+
+# Calculate square
+square = num ** 2
+
+# Print result
+print(f"The square of {num} is {square}")
+
